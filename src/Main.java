735fcaa231d8cf285d289b23c377733e64ff9a70
migsGUIao <miguel_guiao@dlsu.edu.ph>
2023-02-20 14:35:27 +0800

    get indices of start and goal

--- src/Main.java
+++ src/Main.java
@@ -19,14 +19,24 @@ public class Main {
         scan.close();
 
         int maze[][] = mazeInput.getMaze();
+        int x1 = -1, x2 = -1, y1 = -1, y2 = -1;
         
         for (int i = 0; i < maze.length; i++){
             for (int j = 0; j < maze[i].length; j++){
+                if (maze[i][j] == 2) {
+                    maze[i][j] = 9;
+                    x1 = i;
+                    y1 = j;
+                } if (maze[i][j] == 3) {
+                    x2 = i;
+                    y2 = j;
+                }
                 System.out.print(maze[i][j] + " ");
             }
             System.out.println();
         }
-        
 
+        System.out.println("Start: X = " + x1 + " Y = " + y1);
+        System.out.println("End: X = " + x2 + " Y = " + y2);
     }
 }
