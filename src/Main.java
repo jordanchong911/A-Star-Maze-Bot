a7859f53087efae9e8c5e12241993d31bfc4c4de
migsGUIao <miguel_guiao@dlsu.edu.ph>
2023-02-26 02:49:13 +0800

    code that determines if theresno solution(notdone)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-package src;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.lang.Math;
 
@@ -21,10 +20,15 @@ public class Main {
 
         int maze[][] = mazeInput.getMaze();
         int x1 = -1, x2 = -1, y1 = -1, y2 = -1;
+        ArrayList<Integer> walls_x = new ArrayList<>();
+        ArrayList<Integer> walls_y = new ArrayList<>();
 
         for (int i = 0; i < maze.length; i++){
             for (int j = 0; j < maze[i].length; j++){
-                if (maze[i][j] == 2) {
+                if (maze[i][j] == 1) {
+                    walls_x.add(j);
+                    walls_y.add(i);
+                } if (maze[i][j] == 2) {
                     x1 = j;
                     y1 = i;
                 } if (maze[i][j] == 3) {
@@ -41,7 +45,38 @@ public class Main {
         System.out.println("End: X = " + x2 + " Y = " + y2);
         System.out.println("Manhattan distance: " + (Math.abs(x1 - x2) + Math.abs(y1 - y2)) + "\n");
 
+        System.out.println();
+        System.out.println("x");
+        System.out.println(walls_x);
+        System.out.println("-----------------------");
+        System.out.println("y");
+        System.out.println(walls_y);
+
+        int blocked = 0;
+        for (int i = 0; i < walls_x.size(); i++) {           
+            int startState = maze[walls_y.get(i)][walls_x.get(i)];
+
+            if (startState == maze[walls_y.get(i)][walls_x.get(i)+1]) { // check right move
+                continue;
+            }
+
+            if (startState == maze[walls_y.get(i)][walls_x.get(i)-1]) { // check left  move
+                continue;
+            }
+
+            if (startState == maze[walls_y.get(i)+1][walls_x.get(i)]) { // check down move
+                continue;
+            }
+
+            if (startState == maze[walls_y.get(i)-1][walls_x.get(i)]) { // check up move
+                continue;
+            }
+        }
+
         Search search = new Search(maze, x1, y1, x2, y2);
         search.search();
+       
+        
+        
     }
 }
