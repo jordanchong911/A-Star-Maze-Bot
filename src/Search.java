597430d85106b954b942c5a163adff8c5f3d7fff
ReyObejero <rey_obejero@dlsu.edu.ph>
2023-02-25 12:54:35 +0800

    Added a method for finding the next possible actions and a method for getting the solution from the start to the goal

--- src/Search.java
+++ src/Search.java
@@ -8,12 +8,14 @@ public class Search {
         int y;
         int g;
         int h;
+        Position previous_position;
 
         public Position(int x, int y) {
             this.x = x;
             this.y = y;
             this.g = 0;
             this.h = 0;
+            this.previous_position = null;
         }
 
         public int get_f(){
@@ -40,13 +42,13 @@ public class Search {
     }
 
     public ArrayList<Position> search(){
-        ArrayList<Position> path = new ArrayList<>();
+        //ArrayList<Position> path = new ArrayList<>();
 
         while(!unexplored.isEmpty()){
             Position current_position = unexplored.poll();
 
             if(current_position.x == current_position.y && current_position.y == goal_position.y) {
-                for (int i = 0; i < maze.length; i++){
+                /*for (int i = 0; i < maze.length; i++){
                     for (int j = 0; j < maze[i].length; j++)
                         System.out.print(maze[i][j] + " ");
 
@@ -59,36 +61,49 @@ public class Search {
                     if(i<path.size()-1){
                         System.out.print("->");
                     }
+                }*/
+
+                ArrayList<Position> solution = new ArrayList<>();
+                solution = get_solution(current_position);
+
+                for(Position position : solution)
+                    maze[position.y][position.x] = 9;
+
+                for (int i = 0; i < maze.length; i++){
+                    for (int j = 0; j < maze[i].length; j++)
+                        System.out.print(maze[i][j] + " ");
+
+                    System.out.println();
                 }
 
-                return path;
+                for(int i=0; i<solution.size(); i++){
+                    System.out.print("(" + solution.get(i).x + "," + solution.get(i).y + ")");
+
+                    if(i<solution.size()-1){
+                        System.out.print("->");
+                    }
+                }
+
+                return get_solution(current_position);
             }
 
             explored.add(current_position);
-            path.add(current_position);
+            //path.add(current_position);
 
-            for(Position position : path)
-                maze[current_position.y][current_position.x] = 9;
+            /*for(Position position : path)
+                maze[current_position.y][current_position.x] = 9;*/
 
-            for(int[] direction : directions) {
-                int next_x = current_position.x + direction[0];
-                int next_y = current_position.y + direction[1];
+            for(Position next_position : get_next_position(current_position)) {
+                int temp_g = current_position.g + 1;
 
-                // Check if move is valid
-                if (next_y >= 0 && next_y < maze.length
-                        && next_x >= 0 && next_x < maze[0].length
-                        && maze[next_x][next_y] != 1) {
-                    Position next_position = new Position(next_x, next_y);
-                    int temp_g = current_position.g + 1;
+                if (explored.contains(next_position))
+                    continue;
 
-                    if (explored.contains(next_position))
-                        continue;
-
-                    if(!unexplored.contains(next_position) || temp_g < next_position.g){
-                        next_position.g = temp_g;
-                        next_position.h = manhattan_distance(next_position, goal_position);
-                        unexplored.add(next_position);
-                    }
+                if(!unexplored.contains(next_position) || temp_g < next_position.g){
+                    next_position.g = temp_g;
+                    next_position.h = manhattan_distance(next_position, goal_position);
+                    next_position.previous_position = current_position;
+                    unexplored.add(next_position);
                 }
             }
 
@@ -99,5 +114,33 @@ public class Search {
 
     private int manhattan_distance(Position pos1, Position pos2){
         return Math.abs(pos1.x - pos2.x) + Math.abs(pos1.y - pos2.y);
+    }
+
+    private ArrayList<Position> get_next_position(Position current_position){
+        ArrayList<Position> next_positions = new ArrayList<>();
+
+        for(int[] direction : directions){
+            int next_x = current_position.x + direction[0];
+            int next_y = current_position.y + direction[1];
+
+            if (next_x >= 0 && next_x < maze[0].length
+                    && next_y >= 0 && next_y < maze.length
+                    && maze[next_x][next_y] != 1){
+                Position next_position = new Position(next_x, next_y);
+                next_positions.add(next_position);
+            }
+        }
+        return next_positions;
+    }
+
+    private ArrayList<Position> get_solution(Position current_position){
+        ArrayList<Position> path = new ArrayList<>();
+
+        while(current_position != null){
+            path.add(0, current_position);
+            current_position = current_position.previous_position;
+        }
+
+        return path;
     }
 }
